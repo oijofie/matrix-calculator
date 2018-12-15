8334972fffc4c509e0006414e7ff54666951dc35
oijofie <oijofie@gmail.com>
2018-12-14 22:13:07 -0800

    commit

--- matrix calculator/main.cpp
+++ matrix calculator/main.cpp
@@ -2,14 +2,32 @@
 //  main.cpp
 //  matrix calculator
 //
-//  Created by RintaroYamauchi on 12/14/18.
+//  Created by RintaroYamauchi on 12/13/18.
 //  Copyright © 2018 Rintaro Yamauchi. All rights reserved.
 //
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main()
+{
+    int a11, a12, a21, a22;
+    int b11, b12, b21, b22;
+    
+    cout << "matrix calculator" << endl;
+    cout << "-------------------" << endl;
+    cout << " | a11 | a12 |  |  |" << endl;
+    cout << "-------------------" << endl;
+    cout << " | a21 | a22 |  |  |" << endl;
+    cout << "-------------------" << endl;
+    
+    cout << "input numbers for a11, a12, a21, a22" << endl;
+    cout << "inpu numbers for b11, b12, b21, b22" << endl;
+    cin >> a11 >> a12 >> a21 >> a22;
+    cin >> b11 >> b12 >> b21 >> b22;
+    
 }
+
+// make function for matrix to print out
+// make function to calculate matrix such as A*B, inverse, a*ab = a*b, i
