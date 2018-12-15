9be4ce961add8487292403f1bd6c55225e75c0e3
oijofie <oijofie@gmail.com>
2018-12-14 22:08:22 -0800

    Initial Commit

---
+++ matrix calculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  matrix calculator
+//
+//  Created by RintaroYamauchi on 12/14/18.
+//  Copyright © 2018 Rintaro Yamauchi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
