a42663ea31fea68ae2db5abf5e8514cd5d5f1032
oijofie <oijofie@gmail.com>
2018-12-15 19:02:25 -0800

    described a matrix as an array. It might be better to be a linked list?

--- matrix calculator/main.cpp
+++ matrix calculator/main.cpp
@@ -10,10 +10,21 @@
 
 using namespace std;
 
+void matrix(int a11, int a12, int a21, int a22)
+{
+    int array[4] = {a11, a12, a21, a22};
+    cout << "matrix calculator" << endl;
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "| " << array[i];
+        if (i == 1)
+            cout << endl << "-------" << endl;
+    }
+}
+
 int main()
 {
     int a11, a12, a21, a22;
-    int b11, b12, b21, b22;
     
     cout << "matrix calculator" << endl;
     cout << "-------------------" << endl;
@@ -23,11 +34,14 @@ int main()
     cout << "-------------------" << endl;
     
     cout << "input numbers for a11, a12, a21, a22" << endl;
-    cout << "inpu numbers for b11, b12, b21, b22" << endl;
     cin >> a11 >> a12 >> a21 >> a22;
-    cin >> b11 >> b12 >> b21 >> b22;
+    matrix(a11, a12, a21, a22);
+   
     
 }
 
 // make function for matrix to print out
 // make function to calculate matrix such as A*B, inverse, a*ab = a*b, i
+
+
+
